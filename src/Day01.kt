917efc5b5d4c5e314adb26b428178ffcf7c967d0
Erik Nelson <erik.david.nelson@gmail.com>
2022-12-01 00:14:37 -0500

    Initial Day01 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,39 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var elfCal = 0
+        var totalCal = 0
+
+        for(line in input) {
+            val cal = line.toIntOrNull()
+            if(cal == null) {
+                if(elfCal > totalCal) totalCal = elfCal
+                elfCal = 0
+            } else {
+                elfCal += cal
+            }
+        }
+
+        return totalCal
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        var elfCal = 0
+        val calArray = arrayListOf<Int>()
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        for(line in input) {
+            val cal = line.toIntOrNull()
+            if(cal == null) {
+                calArray.add(elfCal)
+                elfCal = 0
+            } else {
+                elfCal += cal
+            }
+        }
+
+        calArray.sortDescending()
+
+        return calArray[0] + calArray[1] + calArray[2]
+    }
 
     val input = readInput("Day01")
     println(part1(input))
