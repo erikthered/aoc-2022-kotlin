188c1fdb907211c77fb6b14e1e9f4ad586bf0b36
Erik Nelson <erik.david.nelson@gmail.com>
2022-12-14 23:42:22 -0500

    Day13 part 1

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,3 +1,10 @@
+
+enum class Order {
+    CORRECT,
+    INCORRECT,
+    UNDECIDED
+}
+
 fun main() {
     fun parsePacketData(line: String): Pair<List<Any>, Int> {
         val data = mutableListOf<Any>()
@@ -31,12 +38,39 @@ fun main() {
         return data to idx
     }
 
+
+    fun checkOrder(left: Any, right: Any) : Order {
+        when {
+            left is Int && right is Int -> {
+                return when {
+                    left < right -> Order.CORRECT
+                    left > right -> Order.INCORRECT
+                    else -> Order.UNDECIDED
+                }
+            }
+            left is List<*> && right is List<*> -> {
+                left.mapIndexed { index, value ->
+                    if (index > right.lastIndex) return Order.INCORRECT
+                    val order = checkOrder(value!!, right[index]!!)
+                    if(order != Order.UNDECIDED) return order
+                }
+                if(left.size < right.size) return Order.CORRECT
+                return Order.UNDECIDED
+            }
+            left is Int && right is List<*> -> return checkOrder(listOf(left), right)
+            left is List<*> && right is Int -> return checkOrder(left, listOf(right))
+        }
+        return Order.UNDECIDED
+    }
+
     fun part1(input: List<String>): Int {
         val packets = input.windowed(2, 3).mapIndexed { packet, (left, right) ->
             packet to (parsePacketData(left).first to parsePacketData(right).first)
         }.toMap()
 
-        return 0
+        val ordering = packets.mapValues { e -> checkOrder(e.value.first, e.value.second) }
+
+        return ordering.filter { it.value == Order.CORRECT }.keys.sumOf { it + 1 }
     }
 
     fun part2(input: List<String>): Int {
@@ -48,8 +82,8 @@ fun main() {
     check(part1(testInput) == 13)
 //    check(part2(testInput) == 70)
 
-//    val input = readInput("Day13")
-//    println(part1(input))
+    val input = readInput("Day13")
+    println(part1(input))
 //    println(part2(input))
 }
 
